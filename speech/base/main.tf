7bcbb4e426fe6062b7b20ffb83a7f40ecc507d72
Mark Tinderholt <mark@vitalsigyn.com>
2020-05-14 18:50:12 -0400

    clean up naming remove location suffix

--- speech/base/main.tf
+++ speech/base/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cognitive_account" "cognitive_speech" {
-  name                = "${var.name}-speech"
+  name                = "${var.name}"
   resource_group_name = var.resource_group_name
   location            = var.location
   kind                = "SpeechServices"
